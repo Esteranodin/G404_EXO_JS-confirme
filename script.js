d0a991d16324e873d77fb35466ee234f5f6e3c40
esteranodin <esteranodin@hotmail.fr>
2024-11-15 12:40:45 +0100

    DONE : exo 4

---
+++ script.js
@@ -0,0 +1,44 @@
+// EXO 1
+// const star = "*";
+// let pyramide = "";
+
+// for (let i = 0; i < 6; i += 1) {
+//     pyramide += star
+//     console.log(pyramide  + '\n');
+// }
+
+// EXO 1 opti/refacto
+// let star = '*';
+
+// for (let i = 0; i < 6; i += 1) {
+//     console.log(star + '\n');
+//      star += star[i];     
+// }
+
+//EXO 2
+// const supersHeros = ["Jessica Jones", "DrStrange", "Batman", "Daredevil"];
+// supersHeros.forEach(
+//     (superHero) => {
+//         console.log("Je suis fan de " + superHero)
+//     }
+// );
+
+//EXO 3
+// let a = 10;
+// while (a >= 0) {
+//      console.log(a);
+//      a -= 1;    
+// }
+// console.log("Décollage !");
+
+//EXO 4
+// const friendsList = ["Olivier", "Laouïg", "Stéphane"];
+// let a = 0;
+// for (let i = 0; i < friendsList.length; i += 1) {
+//      while (a <= i) {
+//           a += 1;
+//           console.log(friendsList[i]);
+//      }
+//      a = 0;
+// };
+
